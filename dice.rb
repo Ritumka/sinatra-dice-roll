d13d8cec6a04796b7ad543cd340db290e7f4ab1f
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-01-28 06:37:26 +0000

    added view templates

--- dice.rb
+++ dice.rb
@@ -8,9 +8,8 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-
 get("/") do
-  "Hello World"
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -26,10 +25,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -37,19 +35,17 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+ erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-	
-  outcome = "You rolled a #{first_die}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -60,8 +56,19 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + forth_die + fifth_die
 	
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{forth_die} and a #{fifth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{forth_die} and a #{fifth_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six)
 end
