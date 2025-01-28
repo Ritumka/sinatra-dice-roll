7d07719f0336b2ff255668ebd2384c17e356c7cd
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-01-28 05:35:21 +0000

    added links to sinatra app

--- dice.rb
+++ dice.rb
@@ -8,62 +8,60 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+
 get("/") do
-  erb(:elephant)
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
 get("/dice/2/6") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
 
 get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
 
 get("/dice/1/20") do
-  @die = rand(1..20)
-
-  @outcome = "You rolled #{@die}."
-  
-  erb(:one_twenty)
+  first_die = rand(1..20)
+	
+  outcome = "You rolled a #{first_die}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
 
 get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
-get("/dice/100/6") do
-  @rolls = []
-
-  100.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_hundred_six)
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  forth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + forth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{forth_die} and a #{fifth_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
